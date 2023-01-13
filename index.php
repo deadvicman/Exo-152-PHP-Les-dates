6a303e7486527c242939dfcb451fb84ff93cbb3d
victorien <deadvicmangame@gmail.com>
2023-01-13 14:04:18 +0100

    les dates php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 
 // Premiere ligne
+$today = date("D");
+echo $today;
+
+echo "<br><br>";
 
 
+echo date("Y.m.d",mktime(0,0,0,12,10,2018));
+
+echo"<br><br>";
+
+echo date("H:i:s",mktime(11,35,07));
 
 
 
